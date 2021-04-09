731fc81c296bccca1bb391071b1337095d7365b2
ChenJack2 <>
2021-04-09 07:06:00 +0000

    cover 1 to 10

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-export default {
+export default function romanArabicConvert (arabic) {
+  let result = ""
+  
+  if (arabic === 10) {
+    return "X"
+  }
 
+  if (arabic >= 5) {
+    result += "V"
+    arabic -= 5
+  }
+
+  if (arabic === 4) {
+    return "IV"
+  }
+
+  result += "I".repeat(arabic)
+  return result
 }
--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,36 @@
 const expect = require("chai").expect
+import romanArabicConvert from "./index"
 
 describe("Roman-Arabic Converter", function () {
+  it("returns no error from the function", function() {
+    expect(romanArabicConvert).not.to.throw(Error)
+  })
 
+  it("returns I for 1", function() {
+    expect(romanArabicConvert(1)).to.eql("I")
+  })
+
+  it("returns II for 2", function() {
+    expect(romanArabicConvert(2)).to.eql("II")
+  })
+
+  it("returns III for 3", function() {
+    expect(romanArabicConvert(3)).to.eql("III")
+  })
+
+  it("returns IV for 4", function() {
+    expect(romanArabicConvert(4)).to.eql("IV")
+  })
+
+  it("returns V for 5", function() {
+    expect(romanArabicConvert(5)).to.eql("V")
+  })
+
+  it("returns VI for 6", function() {
+    expect(romanArabicConvert(6)).to.eql("VI")
+  })
+
+  it("returns X for 10", function() {
+    expect(romanArabicConvert(10)).to.eql("X")
+  })
 })
